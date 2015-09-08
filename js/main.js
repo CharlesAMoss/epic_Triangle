d6be8ca0b9174907caf97eb135d82d6a6aba2061
Charles Moss & Mike Chastain <student@epicodus.com>
2015-09-08 15:10:26 -0700

    add images finished project

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var triangle = function(x, y, z) {
 
-    var x = parseInt(x);
-    var y = parseInt(y);
-    var z = parseInt(z);
+    // var x = parseInt(x);
+    // var y = parseInt(y);
+    // var z = parseInt(z);
 
     var checkArray = [x,y,z];
 
@@ -31,5 +31,35 @@ var triangle = function(x, y, z) {
 
     }
 
-
 };
+
+$(document).ready(function() {
+    $("form#tri").submit(function(event) {
+        var x = parseInt($("input#x").val());
+        var y = parseInt($("input#y").val());
+        var z = parseInt($("input#z").val());
+        var result = triangle(x, y, z);
+
+        $("#result p").text(result);
+
+        if (result === "This is a scalene triangle" ) {
+            $('#im').html('<img src="images/scalene.jpg" />');
+        }
+        if (result === "This is an isosceles triangle" ) {
+            $('#im').html('<img src="images/isosceles.jpg" />');
+        }
+        if (result === "This is an equilateral triangle" ) {
+            $('#im').html('<img src="images/equilateral.jpg" />');
+        }
+        if (result === "This is not a triangle" ) {
+            $('#im').html('<img src="images/not_tri.jpg" />');
+        }
+
+
+
+
+        $("#result").show();
+        event.preventDefault();
+  });
+
+});
